64c5ec94f558398134f5f115cbae49603a7e4027
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2022-03-31 14:51:01 -0300

    adicionado efeitos, cards de informações, estilização de letras e responsividade

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
-import { Container, TextContainer } from './styles';
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
 
 import picture from '../../assets/henrique.png';
 
@@ -17,6 +17,36 @@ function HomeHero() {
           <h1>Olá</h1>
           <h2>Me chamo Henrique</h2>
         </TextContainer>
+        <InfosContainer>
+          <CodeItem>
+            <span className='comment'>//Minha apresentação</span>
+            <span className='purple'>Infos</span> {'\u007B'}
+            <div>
+              Nome: <span className='blue'>Henrique,</span>
+            </div>
+            <div>
+              Sobrenome: <span className='blue'>Gabriel,</span>
+            </div>
+            <div>
+              Cargo: <span className='blue'>Dev full-stack,</span>
+            </div>
+            <div>
+              Cidade: <span className='blue'>Montes Claros</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+          <CodeItem>
+            
+            <span className='purple'>Tecnologias_utilizadas</span> {'\u007B'}
+            <div>
+              Front-end: <span className='blue'>React,</span>
+            </div>
+            <div>
+              Back-end: <span className='blue'>Laravel</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
       </div>
     </Container>
   );
