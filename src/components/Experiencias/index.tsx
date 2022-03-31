ee95718373e40f18f74f524a6a79942c8a1647c3
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2022-03-31 18:26:00 -0300

    experiencias

---
+++ src/components/Experiencias/index.tsx
@@ -0,0 +1,36 @@
+import SectionTitle from '../SectionTitle';
+import ExperienciaItem from './ExperienciaItem';
+import { Container } from './styles';
+
+
+
+function Experiencias() {
+  return (
+    <Container>
+      <SectionTitle title="1 ano" description="de experiência" />
+
+      <section>
+        <ExperienciaItem 
+          year="2020"
+          title="Primeiro Estágio"
+          description="
+          Comecei meu primeiro estágio como suporte técnico na prefeitura de Montes Claros, 
+          onde tive meu primeiro contato com o mercado de trabalho
+          "
+        />
+        <ExperienciaItem 
+          year="2021"
+          title="Desenvolvedor Laravel - Estágio"
+          description="
+          Comecei minha carreira como desenvolvedor estagiando na Universidade estadual de Montes Claros.
+          Desenvolvi sistemas utilizando Laravel e bootstrap, sendo o cargo que atualmente ocupo
+          "
+        />
+        
+      </section>
+      
+    </Container>
+  );
+};
+
+export default Experiencias;
