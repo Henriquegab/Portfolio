3c60a82ec0af829e61240acf7d708d6e081345ee
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2022-04-08 18:00:51 -0300

    começando a popular projetos

---
+++ src/components/ProjetoCard/index.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react';
+import Link from 'next/link';
+
+import { Container } from './styles';
+
+interface ProjetoCardProps {
+  title: string;
+  type: string;
+  imgUrl: string;
+  slug: string;
+  
+  
+}
+
+function ProjetoCard({ title, type, imgUrl, slug }: ProjetoCardProps) {
+  return (
+    <Container>
+      <Link href={`/projetos/${slug}`}>
+        <a>
+          <div className="overlay">
+            <section>
+              <h1>{title}</h1>
+              <h2>{type}</h2>
+            </section>
+          </div>
+        </a>
+      </Link>
+      
+    </Container>
+  );
+};
+
+export default ProjetoCard;
--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,12 +1,41 @@
 import Header from '../../components/Header';
 import { ProjetosContainer } from '../../styles/ProjetosStyles';
+import picture from '../../assets/AdminCEU.png';
+import ProjetoCard from '../../components/ProjetoCard';
 
 export default function Projetos(){
     return(
         <ProjetosContainer>
             <Header />
             <main className="container">
-                <p>oi</p>
+                <ProjetoCard 
+                title = "Projeto 01"
+                type = "Website"
+                slug = "testf"
+                imgUrl="https://avatars.githubusercontent.com/u/67250181?s=400&u=4750b82eaf738a93546dc67a2b5dfa67ea009a67&v=4"
+
+                />
+                <ProjetoCard 
+                title = "Projeto 01"
+                type = "Website"
+                slug = "testf"
+                imgUrl="https://avatars.githubusercontent.com/u/67250181?s=400&u=4750b82eaf738a93546dc67a2b5dfa67ea009a67&v=4"
+
+                />
+                <ProjetoCard 
+                title = "Projeto 01"
+                type = "Website"
+                slug = "testf"
+                imgUrl="https://avatars.githubusercontent.com/u/67250181?s=400&u=4750b82eaf738a93546dc67a2b5dfa67ea009a67&v=4"
+
+                />
+                <ProjetoCard 
+                title = "Projeto 01"
+                type = "Website"
+                slug = "testf"
+                imgUrl="https://avatars.githubusercontent.com/u/67250181?s=400&u=4750b82eaf738a93546dc67a2b5dfa67ea009a67&v=4"
+
+                />
             </main>
         </ProjetosContainer>
     )
