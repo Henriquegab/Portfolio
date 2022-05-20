c04f80866e19985a0723ca69ab14628b2bd24888
Henrique Gabriel Siqueira da Cruz <henriquepro8@gmail.com>
2022-05-20 15:08:12 -0300

    colocando tecnoligias utilizadas nos projetos

--- src/pages/projetos/adminceu/index.tsx
+++ src/pages/projetos/adminceu/index.tsx
@@ -1,7 +1,13 @@
 import BannerProjeto from '../../../components/BannerProjeto';
+import { AiFillHtml5 } from 'react-icons/ai';
 import Header from '../../../components/Header';
 import { ProjetoContainer } from '../../../styles/ProjetoStyles';
 import adminceu from '../../../assets/AdminCEU.png';
+import ConhecimentoItem from '../../../components/Conhecimentos/ConhecimentoItem';
+import SectionTitle from '../../../components/SectionTitle';
+import { Container } from '../../../components/Experiencias/styles';
+import { FaCss3Alt, FaLaravel } from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io5';
 export default function Projeto(){
     return(
         <ProjetoContainer>
@@ -17,13 +23,26 @@ export default function Projeto(){
 
                 O sistema foi desenvolvido com o Laravel 9, PHP 8 e bootstrapp.
                 </p>
+
+                <Container>
+                <SectionTitle title="Tecnologias usadas" />
+                <section>
+                    <ConhecimentoItem  title="HTML" icon={<AiFillHtml5 />}/>
+                    <ConhecimentoItem title="CSS" icon={<FaCss3Alt />} />
+                    <ConhecimentoItem title="Javascript" icon={<IoLogoJavascript />} />
+                    <ConhecimentoItem title="Laravel 9" icon={<FaLaravel />} />
+                </section>
+            </Container>
+
                 <div>
                     <button type='button'>
                     <a href="https://adminceu.herokuapp.com/">Ver projeto online</a>
                     </button>
                 </div>
-            </main>
             
+            
+
+            </main>
 
         </ProjetoContainer>
     )
