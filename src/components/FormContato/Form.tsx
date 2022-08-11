2183552d11273bc68b4ec7286cc268154e1c77b2
Henrique Gabriel Siqueira da Cruz <henrique.cruz@campus.unimontes.int>
2022-08-11 13:54:06 -0300

    feito o envio de email por api no local host

--- src/components/FormContato/Form.tsx
+++ src/components/FormContato/Form.tsx
@@ -7,16 +7,20 @@ export default function Form(){
 
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
-    const [mensagem, setMensagem] = useState('');
+    const [content, setContent] = useState('');
+    const [title, setTitle] = useState('');
 
     async function handleSubmit(event){
       event.preventDefault();
 
       try {
-        await sendContactMail(nome, email, mensagem);
+        await sendContactMail(nome, email, content, title);
         setNome('');
         setEmail('');
-        setMensagem('');
+        setContent('');
+        setTitle('');
+
+        console.log('deu certo!')
 
       } catch(error){
         console.log(error)
@@ -29,7 +33,7 @@ export default function Form(){
 
                 <Input placeholder="Nome" value={nome} onChange={({target}) => setNome(target.value)} required/>
                 <Input placeholder="E-mail" value={email} onChange={({target}) => setEmail(target.value)} required type="email"/>
-                <TextArea placeholder='Mensagem' value={mensagem} onChange={({target}) => setMensagem(target.value)} required />
+                <TextArea placeholder='Mensagem' value={content} onChange={({target}) => setContent(target.value)} required />
                     <button type='submit'>ENVIAR</button>
 
         </FormContainer>
--- src/services/sendMail.ts
+++ src/services/sendMail.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
-export const sendContactMail = async (name: string, senderMail: string, content: string) => {
+export const sendContactMail = async (name: string, email: string, content: string, title: string) => {
   const data = {
     name,
-    senderMail,
-    content
+    email,
+    content,
+    title: 'Contato do Portifólio',
+    user_email: 'henriquepro8@gmail.com'
   };
 
   try{
-    return await axios.post("/api/contact", data);
+    return await axios.post("http://localhost:8000/api/apimail", data);
 
   } catch(error){
     return error;
